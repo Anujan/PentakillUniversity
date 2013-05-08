75fb190d6970e321914cf9d564318100aed8b7fd
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-05-08 15:34:31 -0400

    I guess some views

--- Gemfile
+++ Gemfile
@@ -23,8 +23,6 @@ end
 
 gem 'jquery-rails'
 gem "twitter-bootstrap-rails", "~> 2.2.6"
-gem "therubyracer"
-gem 'less-rails'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :ign, :server
   # attr_accessible :title, :body
+  def mentor? 
+    return self.mentor > 0
+  end
+  
+  def summoner_verified?
+    return self.summonerid > 0
+  end
+  
+  def summoner_verify
+    #TODO: Runepage verification
+  end
 end
