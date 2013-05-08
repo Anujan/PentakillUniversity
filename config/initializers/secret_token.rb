74274d50af78cec0288a02473586e251c12d7c88
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-05-08 10:15:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PentakillUniversity::Application.config.secret_token = '589fb920a5acae3d9c787d43b8f54b889858370c1ffc4e587b4d28903bab38e47d508f3b6a872e681aa3df9a181e9fc78b016197d6bbaa967a59704961da0111'
