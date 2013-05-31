4d5642840ece6e1c7718f9aadb1c78b5289a7591
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-05-31 18:02:17 -0400

    Deployment

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+require "bundler/capistrano"
+
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/nodejs"
+load "config/recipes/rbenv"
+load "config/recipes/check"
+
+server "198.46.140.247", :web, :app, :db, primary: true
+
+set :user, "anujan"
+set :application, "pentakilluniversity"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "https://anujan@bitbucket.org/anujan/pentakill-university.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
