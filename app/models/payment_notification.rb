c8658b4e29bfe89c7ef4dd7977a75b26f75dbf5d
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-05-31 18:22:00 -0400

    syntax error

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -13,7 +13,7 @@ class PaymentNotification < ActiveRecord::Base
   		req = Request.find(request_id)
   		unless req.nil?
         return Rails.logger.info("PaymentNotification #{id} has the wrong price...\r\nmc_gross_1 = #{params[:mc_gross_1]}\r\nprice = #{req.price}") if (params[:mc_gross_1].to_f != req.price.to_f || params[:mc_currency] != 'USD')
-  			req.student.relationship =  Relationship.create(:mentor => req.mentor, :price => req.price, :payment_id = self.id)
+  			req.student.relationship =  Relationship.create(:mentor => req.mentor, :price => req.price, :payment_id => self.id)
   			self.request_id = req.student.relationship.id
   			self.save!
   			req.destroy
