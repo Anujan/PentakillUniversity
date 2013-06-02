d7972fccb9cff0e96ee6678cb750b6d989ec9018
Anujan Panchadcharam <anujan.panchadcharam@uoit.net>
2013-06-02 12:34:46 -0400

    Revert ":|"
    This reverts commit 1b06c2fd9bfc7be57728e026a36ee15a3b5cb1e2.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ PentakillUniversity::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  # config.cache_store = :redis_store
+  config.cache_store = :redis_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -64,5 +64,5 @@ PentakillUniversity::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.default_url_options = { :host => 'theprovinggrounds.net' }
+  config.action_mailer.default_url_options = { :host => 'anujan.name' }
 end
